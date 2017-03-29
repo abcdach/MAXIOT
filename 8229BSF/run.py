60b126fbe57fa4d7f9131ffa2e3a1574f0379ecb
abcdach <d.chkhaidze@gmail.com>
2017-03-29 22:51:07 +0200

    first commit

---
+++ 8229BSF/run.py
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+#from MAXIOT import *
+import time
+import MAXIOT
+
+
+
+
+try:
+	MAXIOT.START()
+	while 1:
+		time.sleep(1)
+		MAXIOT.SEND("12345")	
+finally:
+	print "SYSTEM Exiting !!!!!"
+	MAXIOT.CLIENT_STATUS  = 0
+	MAXIOT.SATELIT_STATUS = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
