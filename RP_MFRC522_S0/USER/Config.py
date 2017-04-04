f231b047be9d0f3d920f924b52bdb2c943580c99
abcdach <d.chkhaidze@gmail.com>
2017-04-04 13:40:19 +0000

    first commit

---
+++ RP_MFRC522_S0/USER/Config.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import spi
+
+
+Spi_Dev = "/dev/spidev0.0"
+RST_PIN	= 22
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ RP_MFRC522_S1/USER/Config.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import spi
+
+
+Spi_Dev = "/dev/spidev1.1"
+RST_PIN	= 18
+
+
+
+
+
+
+
+
+
+
+
+
