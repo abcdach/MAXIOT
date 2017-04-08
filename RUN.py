4e6cede529520831cfafae05ab8425876000b257
abcdach <d.chkhaidze@gmail.com>
2017-04-08 11:18:11 +0000

    first commit

--- RUN.py
+++ RUN.py
@@ -9,13 +9,15 @@ import time
 DevList = [""]*32
 Data    = [""]*2
 ##############################################
-DevList[0]="0,P,OPp_ILI9341,"
-DevList[1]="0,P,OPp_8229BSF,"
-DevList[2]="0,P,RPp_MFRC522_S0,"
-DevList[3]="0,P,RPp_MFRC522_S1,"
-DevList[4]="0,P,RPp_RELAY,"
-DevList[5]="0,P,Up_Host_IP,"
-DevList[6]="0,C,Uc_MIZ_1,"
+DevList[0]="1,SYS,MAXIOT,"
+DevList[1]="1,GUI,RPb_WebGUI,"
+DevList[2]="0,P,OPp_ILI9341,"
+DevList[3]="0,P,OPp_8229BSF,"
+DevList[4]="0,P,RPp_MFRC522_S0,"
+DevList[5]="0,P,RPp_MFRC522_S1,"
+DevList[6]="0,P,RPp_RELAY,"
+DevList[7]="0,P,Up_Host_IP,"
+DevList[8]="0,C,Uc_MIZ_1,"
 ##############################################
 Path = os.path.dirname(os.path.realpath(__file__))
 #print Path
@@ -72,6 +74,21 @@ while 1:
 							#print comm
 							ddd = commands.getoutput(comm)
 							#print ddd
+						if(screen_type=="GUI"):#C
+							print "START : "+screen_name+" "+screen_argu
+							comm = "screen -dmS "+screen_name+" bash -c 'cd "+Path+"/"+screen_name+"/ && ./RUN.sh "+screen_argu+"'"
+							#print comm
+							ddd = commands.getoutput(comm)
+							#print ddd
+							time.sleep(3)
+						if(screen_type=="SYS"):#
+							#print "START : "+screen_name+" "+screen_argu
+							comm = "screen -dmS "+screen_name+" bash -c '/etc/MAXIOT/MAXIOT_SERVER "+screen_argu+"'"
+							print comm
+							ddd = commands.getoutput(comm)
+							#print ddd
+							time.sleep(7)							
+							#/etc/MAXIOT/MAXIOT_SERVER
 					if(screen_comm=="0"):
 						print "STOP  : "+screen_name				
 						commands.getoutput("screen -S "+screen_name+" -X quit")
