b50b5aad22fd5404ee046ec7c00786f5b0e3afe8
abcdach <d.chkhaidze@gmail.com>
2017-04-10 21:38:44 +0200

    first commit

--- RUN.py
+++ RUN.py
@@ -39,48 +39,57 @@ import ntpath
 MAX_DevList = ["","",""]*256
 MAX_DevNum  = int(0)
 ##############################################
-#print "[OPI : DEVICES]"
-PATH=Dir_Path+"/DEV/OP/"
-Dev_List = glob.glob(PATH+"*")
-for Dev_Path in Dev_List:
-	#print Dev_Path
+for Dev_Path in glob.glob(Dir_Path+"/DEV/OP/*"):
 	Dev_Name = ntpath.basename(Dev_Path)
-	MAX_DevList[MAX_DevNum]=(Dev_Name[0],Dev_Name,Dev_Path)
+	MAX_DevList[MAX_DevNum]=(Dev_Name[0],Dev_Name,Dev_Path+"/")
 	MAX_DevNum=MAX_DevNum+1
-	#print MAX_DevNum
 ##############################################
-#print "[RPI : DEVICES]"
-PATH=Dir_Path+"/DEV/RP/"
-Dev_List = glob.glob(PATH+"*")
-for Dev_Path in Dev_List:
-	#print Dev_Path
+for Dev_Path in glob.glob(Dir_Path+"/DEV/RP/*"):
 	Dev_Name = ntpath.basename(Dev_Path)
-	#print Dev_Name
-	MAX_DevList[MAX_DevNum]=(Dev_Name[0],Dev_Name,Dev_Path)
+	MAX_DevList[MAX_DevNum]=(Dev_Name[0],Dev_Name,Dev_Path+"/")
 	MAX_DevNum=MAX_DevNum+1
-	#print MAX_DevNum
 ##############################################
-#print "[UNI : DEVICES]"
-PATH=Dir_Path+"/DEV/UN/"
-Dev_List = glob.glob(PATH+"*")
-for Dev_Path in Dev_List:
-	#print Dev_Path
+for Dev_Path in glob.glob(Dir_Path+"/DEV/UN/*"):
 	Dev_Name = ntpath.basename(Dev_Path)
-	#print Dev_Name
-	MAX_DevList[MAX_DevNum]=(Dev_Name[0],Dev_Name,Dev_Path)
+	MAX_DevList[MAX_DevNum]=(Dev_Name[0],Dev_Name,Dev_Path+"/")
 	MAX_DevNum=MAX_DevNum+1
-	#print MAX_DevNum
+##############################################
+for i in xrange(MAX_DevNum):
+	print MAX_DevList[i]
+##############################################	
+def Finde_Dev(dev):
+	for i in xrange(MAX_DevNum):
+		if(dev==MAX_DevList[i][1]):
+			_Type = MAX_DevList[i][0]
+			_Name = MAX_DevList[i][1]
+			_Path = MAX_DevList[i][2]
+			return (_Type,_Path)
+	return ("","")
 ##############################################
 
 
-#print MAX_DevList
-for c in xrange(MAX_DevNum):
-	print MAX_DevList[c]
+	
+	
+	
+
+(_Dev_Type,_Dev_Path) = Finde_Dev("pOP_ILI9341")
+if(len(_Dev_Path)>0):
+	print _Dev_Type
+	print _Dev_Path
 
 ##############################################
 
 
-exit(0)
+
+
+
+
+
+
+
+
+
+#exit(0)
 
 
 
@@ -88,37 +97,26 @@ exit(0)
 
 
 ##############################################
-DevList = [""]*32
+DevList = [""]*256
 Data    = [""]*2
 ##############################################
-DevList[0]="1,SYS,MAXIOT,"
-DevList[1]="0,GUI,RPb_WebGUI,"
-DevList[2]="0,P,OPp_ILI9341,"
-DevList[3]="0,P,OPp_8229BSF,"
-DevList[4]="1,P,OPy_BMP180,"
-DevList[5]="0,P,RPp_MFRC522_S0,"
-DevList[6]="0,P,RPp_MFRC522_S1,"
-DevList[7]="0,P,RPp_RELAY,"
-DevList[8]="0,P,Up_Host_IP,"
-DevList[9]="0,C,Uc_MIZ_1,"
+DevList[0]="1,MAXIOT,"
+DevList[1]="0,bRP_WebGUI,"
+DevList[2]="0,pOP_ILI9341,"
+DevList[3]="0,pOP_8229BSF,"
+DevList[4]="1,pOP_BMP180,"
+DevList[5]="0,pRP_MFRC522_S0,"
+DevList[6]="0,pRP_MFRC522_S1,"
+DevList[7]="0,pRP_RELAY,"
+DevList[8]="0,pUN_Host_IP,"
+DevList[9]="0,pUN_MIZ_1,"
 ##############################################
 Path = os.path.dirname(os.path.realpath(__file__))
 #print Path
 ##############################################
-commands.getoutput("screen -S OPp_ILI9341    -X quit")
+commands.getoutput("screen -S OPy_BMP180    -X quit")
 time.sleep(0.2)
-commands.getoutput("screen -S OPp_8229BSF    -X quit")
-time.sleep(0.2)
-commands.getoutput("screen -S RPp_MFRC522_S0 -X quit")
-time.sleep(0.2)
-commands.getoutput("screen -S RPp_MFRC522_S1 -X quit")
-time.sleep(0.2)
-commands.getoutput("screen -S RPp_RELAY      -X quit")
-time.sleep(0.2)
-commands.getoutput("screen -S Up_Host_IP     -X quit")
-time.sleep(0.2)
-commands.getoutput("screen -S Uc_MIZ_1       -X quit")
-time.sleep(1)
+
 ##############################################
 cou = 0
 while 1:
@@ -132,7 +130,7 @@ while 1:
 			if(len(Data)>2):
 				#print Data
 				screen_comm = str(Data[0])
-				screen_type = str(Data[1])
+				#screen_type = str(Data[1])
 				screen_name = str(Data[2])
 				screen_argu = str(Data[3])
 				vel = screen.find(screen_name)
@@ -145,28 +143,34 @@ while 1:
 				#print "screen_stat:"+screen_stat
 				if(screen_comm!=screen_stat):
 					if(screen_comm=="1"):
-						if(screen_type=="P"):#Python
-							print "START : "+screen_name+" "+screen_argu
-							comm = "screen -dmS "+screen_name+" bash -c 'cd "+Path+"/"+screen_name+"/ && python RUN.py "+screen_argu+"'"
-							#print comm
-							ddd = commands.getoutput(comm)
-							#print ddd
-							time.sleep(1)
-						if(screen_type=="C"):#C
-							print "START : "+screen_name+" "+screen_argu
-							comm = "screen -dmS "+screen_name+" bash -c 'cd "+Path+"/"+screen_name+"/ && ./RUN "+screen_argu+"'"
-							#print comm
-							ddd = commands.getoutput(comm)
-							#print ddd
-							time.sleep(1)
-						if(screen_type=="GUI"):#C
-							print "START : "+screen_name+" "+screen_argu
-							comm = "screen -dmS "+screen_name+" bash -c 'cd "+Path+"/"+screen_name+"/ && ./RUN.sh "+screen_argu+"'"
-							#print comm
-							ddd = commands.getoutput(comm)
-							#print ddd
-							time.sleep(3)
-						if(screen_type=="SYS"):#
+					
+						(_Dev_Type,_Dev_Path) = Finde_Dev(screen_name)
+						if(len(_Dev_Path)>0):
+							if(_Dev_Type=="p"):#Python
+								print "START : "+screen_name+" "+screen_argu
+								comm = "screen -dmS "+screen_name+" bash -c 'cd "+_Dev_Path+" && python RUN.py "+screen_argu+"'"
+								print comm
+								ddd = commands.getoutput(comm)
+								#print ddd
+								time.sleep(1)
+							if(screen_type=="c"):#C
+								print "START : "+screen_name+" "+screen_argu
+								comm = "screen -dmS "+screen_name+" bash -c 'cd "+_Dev_Path+" && ./RUN "+screen_argu+"'"
+								#print comm
+								ddd = commands.getoutput(comm)
+								#print ddd
+								time.sleep(1)
+							if(screen_type=="b"):#BASH
+								print "START : "+screen_name+" "+screen_argu
+								comm = "screen -dmS "+screen_name+" bash -c 'cd "+_Dev_Path+" && ./RUN.sh "+screen_argu+"'"
+								#print comm
+								ddd = commands.getoutput(comm)
+								#print ddd
+								time.sleep(3)
+							
+							
+							
+						if(screen_name=="MAXIOT"):#
 							#print "START : "+screen_name+" "+screen_argu
 							comm = "screen -dmS "+screen_name+" bash -c '/etc/MAXIOT/MAXIOT_SERVER "+screen_argu+"'"
 							print comm
